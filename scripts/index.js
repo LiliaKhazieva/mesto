281718a67b6e9e1b2176c0d69689851f9c9dce71
Lilia Khazieva <lilia002777@mail.ru>
2022-11-14 19:12:27 +0500

    fix html

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,32 @@
+ const popUp = document.querySelector('.popup');
  const openPopup = document.querySelector('.profile__edit-button');
  const closePopup = document.querySelector('.popup__close-button');
- const popUp = document.querySelector('.popup');
-
- openPopup.addEventListener('click', function (e){
-   e.preventDefault();
-   popUp.classList.add('active');
- })
-
- closePopup.addEventListener('click', () => {
-   popUp.classList.remove('active');
- });
 
  let formElement = document.querySelector('.popup__form');
+ let nameInput = document.querySelector('.popup__input_type_name');
+ let jobInput = document.querySelector('.popup__input_type_info');
+ let profileTitle = document.querySelector('.profile__title');
+ let profileSubtitle = document.querySelector('.profile__subtitle');
 
- let nameInput = document.querySelector('.input-name');
- let jobInput = document.querySelector('.input-job');
- let popupFormButton = document.querySelector('.popup__form-button');
+ const popupOpen = function () {
+   nameInput.value = profileTitle.textContent;
+   jobInput.value = profileSubtitle.textContent;
 
-   function formSubmitHandler (evt) {
-     evt.preventDefault();
-     nameInput.value;
-     jobInput.value;
+   popUp.classList.add('popup_opened');
+ }
 
-     let profileTitle = document.querySelector('.profile__title');
-     let profileSubtitle = document.querySelector('.profile__subtitle');
+ const popupClose = function () {
+   popUp.classList.remove('popup_opened');
+ }
 
-     profileTitle.textContent = nameInput.value;
-     profileSubtitle.textContent = jobInput.value;
-     popUp.classList.remove('active');
-   }
+ function formSubmitHandler(evt) {
+   evt.preventDefault();
+   profileTitle.textContent = nameInput.value;
+   profileSubtitle.textContent = jobInput.value;
 
+   popUp.classList.remove('popup_opened');
+ }
+
+ openPopup.addEventListener('click', popupOpen);
+ closePopup.addEventListener('click', popupClose);
  formElement.addEventListener('submit', formSubmitHandler);
